a98bf0f45a96c194c4a7d9f2412185b1047b12a0
unknown <priyanka0003a@gmail.com>
2026-03-01 20:16:27 +0530

    1

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
     <>
       <Hero />
 
-      <div className="h-[300vh] bg-gradient-to-b from-black to-gray-900 flex items-center justify-center text-4xl">
+      {/* <div className="h-[300vh] bg-gradient-to-b from-black to-gray-900 flex items-center justify-center text-4xl">
         Scroll Down
-      </div>
+      </div> */}
     </>
   );
 }
